f3909c6cd3b83394d66dfe5f15799fd61d98b7bb
Jeffrey Nichols <jwnichols@us.ibm.com>
2013-01-07 23:50:25 -0800

    Fix a stupid redirect bug when making an access token

--- app/controllers/temp_access_tokens_controller.rb
+++ app/controllers/temp_access_tokens_controller.rb
@@ -13,13 +13,8 @@ class TempAccessTokensController < ApplicationController
     @temp_access_token = TempAccessToken.new()
     @temp_access_token.token = SecureRandom.urlsafe_base64.to_s
     @temp_access_token.user = current_user
-
-    respond_to do |format|
-      if @temp_access_token.save
-        redirect_to temp_access_tokens_path
-      else
-        format.html { render action: "new" }
-      end
-    end
+    @temp_access_token.save
+    
+    redirect_to temp_access_tokens_path
   end
 end
