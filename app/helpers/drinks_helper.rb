be6af936dd3f6368157ce767ee312df2b5162493
Jeffrey Nichols <jwnichols@us.ibm.com>
2013-02-19 00:17:30 -0800

    An initial attempt at abstracting the auto-linking code for the recipe template.  Not quite working yet.

--- app/helpers/drinks_helper.rb
+++ app/helpers/drinks_helper.rb
@@ -1,2 +1,16 @@
 module DrinksHelper
+  def render_with_links(text)
+    output = ""
+    chunks = text.split(/(http\:\/\/\S+)/)
+		chunks.each { |c|
+  		m = c.match(/(http\:\/\/\S+)/)
+  		if m
+  			output += "<a href=\"" + h(m[1]) + "\">" + h(m[1]) + "</a>"
+  		else
+  			output += h(c)
+  		end
+		}
+		
+		output
+  end
 end
