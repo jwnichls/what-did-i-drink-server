a9fc0df19b4dfcc030e5dffc366003bca24cb3e4
Jeffrey Nichols <jwnichols@us.ibm.com>
2013-07-11 22:54:21 -0700

    Ensure that the autocomplete manipulation script is loaded only once and changes the autocomplete menu only once

--- public/javascripts/drink-autocomplete.js
+++ public/javascripts/drink-autocomplete.js
@@ -1,36 +1,54 @@
-var autoCompleteInit = function () {
-
-	//alert("initializing drink autocomplete" + $("#drinksearch")[0])
-
-	$("#drinksearch").on("autocompleteresponse", function(evt,ui) {
+if (autoCompleteInit == undefined)
+{
+	// only execute this once
 	
-		var drinkName = $("#drinksearch")[0].value
-	
-		ui.content.push({
-			label: "Add new drink \"" + drinkName + "\"...", 
-			value: drinkName,
-			addDrink: drinkName
+	var autoCompleteInit = function () {
+
+		//alert("initializing drink autocomplete" + $("#drinksearch")[0])
+
+		$("#drinksearch").on("autocompleteresponse", function(evt,ui) {
+
+			var drinkName = $("#drinksearch")[0].value
+
+			var found = false;
+			for(var i = 0; i < ui.content.length; i++)
+			{
+				if (ui.content[i].value == drinkName)
+				{
+					found = true;
+					break;
+				}
+			}
+
+			if (!found && ui.content.length > 0 && ui.content[ui.content.length-1].value != drinkName)
+			{
+				ui.content.push({
+					label: "Add new drink \"" + drinkName + "\"...", 
+					value: drinkName,
+					addDrink: drinkName
+				});
+			}
+
 		});
-	
-	});
 
-	$("#drinksearch").on("autocompleteselect", function(evt,ui) {
-	
-		if (ui.item.addDrink)
-		{
-			evt.preventDefault();
-			evt.stopImmediatePropagation();
+		$("#drinksearch").on("autocompleteselect", function(evt,ui) {
 
-			// open the new drink web page
-			window.open("/drinks/new?name=" + encodeURIComponent(ui.item.addDrink), "_self")
-		}
-	
-	});
+			if (ui.item.addDrink)
+			{
+				evt.preventDefault();
+				evt.stopImmediatePropagation();
 
-};
+				// open the new drink web page
+				window.open("/drinks/new?name=" + encodeURIComponent(ui.item.addDrink), "_self")
+			}
 
-// initialize for desktop
-$(autoCompleteInit);
+		});
 
-// initialize for jQuery Mobile
-$(document).bind('pageinit', autoCompleteInit);
+	};
+
+	// initialize for desktop
+	$(autoCompleteInit);
+
+	// initialize for jQuery Mobile
+	$(document).bind('pageinit', autoCompleteInit);	
+}
