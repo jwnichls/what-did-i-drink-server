d9a99738464d37aba480ffbd8e3021650993ba70
Jeffrey Nichols <jeff@jeffreynichols.com>
2013-10-17 11:18:00 -0400

    Add some required controller parameters

--- app/controllers/v1/venues_controller.rb
+++ app/controllers/v1/venues_controller.rb
@@ -1,5 +1,7 @@
 module V1
   class VenuesController < ApplicationController
+    respond_to :json
+    doorkeeper_for :all, :except => [:show, :index, :search]
 
     # GET /venues
     # GET /venues.json
